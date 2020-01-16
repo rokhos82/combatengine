f773713295aa40dda31783498766e5cd78b2b1e4
Justin Lane <justin.l.lane@gmail.com>
2020-01-15 17:02:21 -0700

    Work on conditions and simulate UI

--- app/ce.simulate.js
+++ app/ce.simulate.js
@@ -6,7 +6,9 @@
     $this = this;
 
     $this.appVersion = _appVersion;
-    $this.teams = {}
+    $this.teams = {};
+    $this.armed = false;
+    $this.status = "Ready for setup! Please review the teams below and click 'Setup Combat' when ready to begin.";
 
     $this.$onInit = function() {
       _log.info("Entering the simulate state");
@@ -14,7 +16,19 @@
       _state.title = "Simulate";
       $this.teams["Red"] = _groups.list("Red");
       $this.teams["Blue"] = _groups.list("Blue");
+
+      if (!readyCheck()) {
+        $this.status = "Both teams must have at least one unit!";
+      }
       //$scope.$apply();
+    };
+
+    function readyCheck() {
+      return ($this.teams["Red"].units && $this.teams["Blue"].units);
+    }
+
+    $this.setup = function() {
+      $this.armed = true;
     };
 
     $this.start = function() {
