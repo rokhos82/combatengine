0952b2cd9c2031a2005798ba8ed09ab048221b78
Justin Lane <justin.l.lane@gmail.com>
2020-02-12 16:50:37 -0700

    Sim UI Update 5
    Simulation Setup UI almost done

--- app/sim/sim.setup.js
+++ app/sim/sim.setup.js
@@ -5,9 +5,23 @@
   //
   //
   //
-  function controller() {}
+  function controller(_uuid) {
+    var $this = this;
 
-  controller.$inject = [];
+    $this.settings = {};
+
+    $this.ui = {
+      turns: false,
+      domination: false
+    };
+
+
+    $this.$onInit = function() {
+      $this.settings.uuid = _uuid.generate();
+    };
+  }
+
+  controller.$inject = ["UuidService"];
 
   angular.module("ce.app").component("ce.app.simulate.setup",{
     templateUrl: "./app/sim/sim.setup.html",
